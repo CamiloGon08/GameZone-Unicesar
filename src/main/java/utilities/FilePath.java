0dd82eccb0b78edfcfdaa394b623076a99233fac
estefaniamarquez <estefaniamarquez@unicesar.edu.co>
2026-09-16 09:41:54 -0500

    feat: Create a new package for utilities and a new class for the file paths.

---
+++ src/main/java/utilities/FilePath.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class FilePath {
+    private static final String PERSONS = "persons.txt";
+    private static final String SALES = "sales.txt";
+    private static final String PRODUCTS = "products.txt";
+    private static final String RETURNS = "returns.txt";
+    private static final String WARRANTIES = "warranties.txt";
+    private static final String PROMOTIONS = "promotions.txt";
+}
