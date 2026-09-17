b241149b3f74746234d4fb76eb133a0067ede139
JesusDominguez <jdaviddominguez@unicesar.edu.co>
2026-09-17 10:58:25 -0500

    feat: add Controller accessory class

---
+++ src/main/java/com/mycompany/gamezone/model/Controller.java
@@ -0,0 +1,31 @@
+package com.gamezone.model;
+
+import com.mycompany.gamezone.model.Accesory;
+
+public class Controller extends Accessory {
+
+    private String connectionType;
+
+    
+    public Controller(String id, String title, double price, int stock, String connectionType) {
+        super(id, title, price, stock);
+        this.connectionType = connectionType;
+    }
+
+    
+    @Override
+    public String getDescription() {
+        return String.format("%s [Controller] - Connection: %s, Price: %.2f, Stock: %d",
+                getTitle(), connectionType, getPrice(), getStock());
+    }
+
+    
+    public String getConnectionType() {
+        return connectionType;
+    }
+
+    
+    public void setConnectionType(String connectionType) {
+        this.connectionType = connectionType;
+    }
+}
