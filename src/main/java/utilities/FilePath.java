f7e76bb573a532cfbfbfd9bf4c0a0d1787686d24
estefaniamarquez <estefaniamarquez@unicesar.edu.co>
2026-09-17 16:34:39 -0500

    fix: Correct the spelling of "Accessory"

--- src/main/java/utilities/FilePath.java
+++ src/main/java/utilities/FilePath.java
@@ -4,7 +4,7 @@ public class FilePath {
     public static final String PERSONS = "persons.txt";
     public static final String SALES = "sales.txt";
     public static final String PRODUCTS = "products.txt";
-    public static final String ACCESORIES = "accesories.txt";
+    public static final String ACCESSORIES = "accesories.txt";
     public static final String RETURNS = "returns.txt";
     public static final String WARRANTIES = "warranties.txt";
     public static final String PROMOTIONS = "promotions.txt";
